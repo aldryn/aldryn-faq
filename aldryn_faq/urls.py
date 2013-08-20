014e8fff92c73780fd653273f45059e5e4c44ccf
Patrick Lauber <digi@treepy.com>
2013-08-20 13:05:14 +0200

    version bump

--- aldryn_faq/urls.py
+++ aldryn_faq/urls.py
@@ -6,5 +6,5 @@ from aldryn_faq.views import FaqByCategoryView, FaqAnswerView
 urlpatterns = patterns(
     '',
     url(r'^(?P<category_slug>[-\w]+)/$', FaqByCategoryView.as_view(), name='faq-category'),
-    url(r'^(?P<category_slug>[-\w]+)/(?P<id>[0-9]+)$', FaqAnswerView.as_view(), name='faq-answer'),
+    url(r'^(?P<category_slug>[-\w]+)/(?P<id>[0-9]+)/$', FaqAnswerView.as_view(), name='faq-answer'),
 )
