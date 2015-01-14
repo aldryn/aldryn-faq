b0c3c0c961cb708271292133b752e4eb85706254
Martin Koistinen <mkoistinen@gmail.com>
2015-01-15 00:12:54 +0200

    Setup testing config

---
+++ test_settings.py
@@ -0,0 +1,15 @@
+HELPER_SETTINGS = {
+    'TIME_ZONE': 'Europe/Zurich',
+    'LANGUAGES': (
+        ('en', 'English'),
+        ('de', 'German'),
+        ('fr', 'French'),
+    ),
+    'INSTALLED_APPS': [
+        'adminsortable',
+        'aldryn_faq',
+        'djangocms_text_ckeditor',
+        'hvad',
+        'sortedm2m',
+    ],
+}
