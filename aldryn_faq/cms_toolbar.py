3cb45d8624753bd1095b9f6e8fbc436fa6675fa4
Martin Koistinen <mkoistinen@gmail.com>
2015-01-15 17:24:39 +0200

    pep8 80col

--- aldryn_faq/cms_toolbar.py
+++ aldryn_faq/cms_toolbar.py
@@ -5,7 +5,10 @@ from django.utils.translation import ugettext_lazy as _
 from cms.toolbar_pool import toolbar_pool
 from cms.toolbar_base import CMSToolbar
 
-from aldryn_faq import request_faq_category_identifier, request_faq_question_identifier
+from aldryn_faq import (
+    request_faq_category_identifier,
+    request_faq_question_identifier,
+)
 
 
 @toolbar_pool.register
@@ -14,23 +17,35 @@ class FaqToolbar(CMSToolbar):
     def populate(self):
 
         def can(action, model):
-            perm = 'aldryn_faq.%(action)s_%(model)s' % {'action': action, 'model': model}
+            perm = 'aldryn_faq.%(action)s_%(model)s' % {
+                'action': action, 'model': model}
             return self.request.user.has_perm(perm)
 
-        if self.is_current_app and (can('add', 'category') or can('change', 'category')):
+        if self.is_current_app and (
+                can('add', 'category') or can('change', 'category')):
             menu = self.toolbar.get_or_create_menu('faq-app', _('FAQ'))
 
             if can('add', 'category'):
-                menu.add_modal_item(_('Add category'), reverse('admin:aldryn_faq_category_add'))
+                menu.add_modal_item(
+                    _('Add category'),
+                    reverse('admin:aldryn_faq_category_add')
+                )
 
-            category = getattr(self.request, request_faq_category_identifier, None)
+            category = getattr(self.request,
+                request_faq_category_identifier, None)
 
             if category and can('add', 'question'):
                 params = ('?category=%s&language=%s' %
                           (category.pk, self.request.LANGUAGE_CODE))
-                menu.add_modal_item(_('Add question'), reverse('admin:aldryn_faq_question_add') + params)
+                menu.add_modal_item(
+                    _('Add question'),
+                    reverse('admin:aldryn_faq_question_add') + params
+                )
             if category and can('change', 'category'):
-                url = reverse('admin:aldryn_faq_category_change', args=(category.pk,))
+                url = reverse(
+                    'admin:aldryn_faq_category_change',
+                    args=(category.pk,),
+                )
                 menu.add_modal_item(_('Edit category'), url, active=True)
 
             question = getattr(self.request, request_faq_question_identifier, None)
