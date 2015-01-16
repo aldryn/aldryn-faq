6a02792558796eef86fa3d754af87c43f209b36e
Martin Koistinen <mkoistinen@gmail.com>
2015-01-16 12:07:58 +0200

    pep8, 80col

--- aldryn_faq/urls.py
+++ aldryn_faq/urls.py
@@ -5,6 +5,14 @@ from .views import FaqByCategoryView, FaqAnswerView
 
 
 urlpatterns = patterns('',
-    url(r'^(?P<category_slug>[-\w]+)/$', FaqByCategoryView.as_view(), name='faq-category'),
-    url(r'^(?P<category_slug>[-\w]+)/(?P<pk>[0-9]+)/$', FaqAnswerView.as_view(), name='faq-answer'),
+    url(
+        r'^(?P<category_slug>[-\w]+)/$',
+        FaqByCategoryView.as_view(),
+        name='faq-category'
+    ),
+    url(
+        r'^(?P<category_slug>[-\w]+)/(?P<pk>[0-9]+)/$',
+        FaqAnswerView.as_view(),
+        name='faq-answer'
+    ),
 )
