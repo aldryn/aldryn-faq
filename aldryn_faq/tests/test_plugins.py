45017a5c0ed1c13edb22225131638d17c9a7a67e
Martin Koistinen <mkoistinen@gmail.com>
2015-01-16 14:22:33 +0200

    Remaining plugin tests

--- aldryn_faq/tests/test_plugins.py
+++ aldryn_faq/tests/test_plugins.py
@@ -13,14 +13,12 @@ from . import AldrynFaqTest, CMSRequestBasedTest
 
 
 class PluginBaseTest(AldrynFaqTest, CMSRequestBasedTest):
-
     def setUp(self):
         super(PluginBaseTest, self).setUp()
         pass
 
 
 class TestQuestionListPlugin(PluginBaseTest):
-
     # TODO, this plugin doesn't appear to work!
     pass
     # def test_plugin(self):
@@ -48,6 +46,54 @@ class TestLatestQuestionsPlugin(PluginBaseTest):
         url1 = self.reload(self.question1, "en").get_absolute_url()
         url2 = self.reload(self.question2, "en").get_absolute_url()
         rendered = plugin.render_plugin(context, ph)
+        self.assertTrue(rendered.find(url1) > -1)
+        self.assertTrue(rendered.find(url2) > -1)
+        # Test that question2 appears before question1
+        self.assertTrue(rendered.find(url2) < rendered.find(url1))
+
+
+class TestTopQuestionsPlugin(PluginBaseTest):
+    def test_plugin(self):
+        page1 = self.get_or_create_page("Page One")
+        ph = page1.placeholders.get(slot='content')
+        plugin = add_plugin(ph, 'TopQuestionsPlugin', language='en')
+
+        # First test that no plugins are found initially
+        request = self.get_page_request(
+            page1, self.user, None, lang_code='en', edit=False)
+        context = RequestContext(request, {})
+        rendered = plugin.render_plugin(context, ph)
+        self.assertTrue(rendered.find("No entry found") > -1)
+
+        # Now test, set a question to be "top", then test that it appears.
+        self.question1.is_top = True
+        self.question1.save()
+        request = self.get_page_request(
+            page1, self.user, None, lang_code='en', edit=False)
+        context = RequestContext(request, {})
+        question1 = self.reload(self.question1, "en")
+        rendered = plugin.render_plugin(context, ph)
+        self.assertTrue(rendered.find(question1.title) > -1)
+
+
+class TestMostReadQuestionsPlugin(PluginBaseTest):
+    def test_plugin(self):
+        # Prepare the questions...
+        self.question1.number_of_visits = 5
+        self.question1.save()
+        self.question2.number_of_visits = 10
+        self.question2.save()
+
+        page1 = self.get_or_create_page("Page One")
+        ph = page1.placeholders.get(slot="content")
+        plugin = add_plugin(ph, "MostReadQuestionsPlugin", language="en")
+        request = self.get_page_request(
+            page1, self.user, None, lang_code="en", edit=False)
+        context = RequestContext(request, {})
+        url1 = self.reload(self.question1, "en").get_absolute_url()
+        url2 = self.reload(self.question2, "en").get_absolute_url()
+        rendered = plugin.render_plugin(context, ph)
+        # Ensure both questions appear...
         self.assertTrue(rendered.find(url1) > -1)
         self.assertTrue(rendered.find(url2) > -1)
         # Test that question2 appears before question1
