5c1c9869d17cd31769af2fbc821d9e26a3fd8e02
Martin Koistinen <mkoistinen@gmail.com>
2015-03-24 13:20:07 +0200

    Use latest boilerplates

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 from aldryn_faq import __version__
 
 REQUIREMENTS = [
-    'aldryn-boilerplates>=0.6',
+    '-e git+http://github.com/aldryn/aldryn-apphooks-config.git#egg=aldryn-apphooks-config',
     'aldryn-reversion',
     'aldryn-search',
     'django-admin-sortable2',
