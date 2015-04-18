0b741c89ea19759f25526256ee039707cb423cef
Martin Koistinen <mkoistinen@gmail.com>
2015-04-18 05:01:39 +0300

    Fix tests to now include the questions, which are now in the menu

--- aldryn_faq/tests/test_menu.py
+++ aldryn_faq/tests/test_menu.py
@@ -3,33 +3,31 @@
 from __future__ import unicode_literals
 
 from aldryn_faq.menu import FaqCategoryMenu
-from django.utils.translation import (
-    get_language_from_request,
-)
+
 from .test_base import AldrynFaqTest, CMSRequestBasedTest
 
 
 class TestMenu(AldrynFaqTest, CMSRequestBasedTest):
 
     def test_get_nodes(self):
-        # Test that the EN version of the menu has only category1 and is shown
-        # in English.
+        # Test that the EN version of the menu has only category1 and its
+        # question1, and is shown in English.
         request = self.get_page_request(None, self.user, '/en/')
         menu = FaqCategoryMenu()
         category1 = self.reload(self.category1, 'en')
+        question1 = self.reload(self.question1, 'en')
         self.assertEqualItems(
             [menuitem.title for menuitem in menu.get_nodes(request)],
-            [category1.name]
+            [category1.name, question1.title]
         )
 
-        # Test that the DE version has 2 categories and that they are shown in
-        # German.
+        # Test that the DE version has 2 categories and their questions that
+        # they are shown in German.
         request = self.get_page_request(None, self.user, '/de/')
         menu = FaqCategoryMenu()
-        category1 = self.reload(self.category1, 'de')
-        category2 = self.reload(self.category2, 'de')
         nodes = menu.get_nodes(request)
         self.assertEqualItems(
             [menuitem.title for menuitem in nodes],
-            [category1.name, category2.name]
+            [self.category1.name, self.category2.name, self.question1.title,
+                self.question2.title]
         )
