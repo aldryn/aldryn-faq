a807024e8bf50356a5c9f8f052661829481a93ac
Angelo Dini <angelo.dini@divio.ch>
2015-06-24 08:08:09 +0200

    Update __init__.py

--- aldryn_faq/__init__.py
+++ aldryn_faq/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import unicode_literals
 
-__version__ = '0.12.6'
+__version__ = '0.12.7'
 request_faq_category_identifier = 'aldryn_faq_current_category'
 request_faq_question_identifier = 'aldryn_faq_current_question'
