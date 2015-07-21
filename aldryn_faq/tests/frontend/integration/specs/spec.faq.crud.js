7343729d53d023dd795860450ab69f9fcd45f795
Marketionist <marketionist@gmail.com>
2015-07-21 12:49:35 +0300

    Added page creation test

--- aldryn_faq/tests/frontend/integration/specs/spec.faq.crud.js
+++ aldryn_faq/tests/frontend/integration/specs/spec.faq.crud.js
@@ -4,7 +4,7 @@
  */
 
 'use strict';
-/* global describe, it, browser */
+/* global describe, it, browser, By, expect */
 
 // #############################################################################
 // INTEGRATION TEST
@@ -32,6 +32,81 @@ describe('Aldryn FAQ tests: ', function () {
 
             // login to the site
             faqPage.cmsLogin();
+        });
+    });
+
+    it('creates a new test page', function () {
+        // click the example.com link in the top menu
+        faqPage.userMenus.first().click().then(function () {
+            // wait for top menu dropdown options to appear
+            browser.wait(function () {
+                return browser.isElementPresent(faqPage.userMenuDropdown);
+            }, faqPage.mainElementsWaitTime);
+
+            faqPage.administrationOptions.first().click();
+        }).then(function () {
+            // wait for modal iframe to appear
+            browser.wait(function () {
+                return browser.isElementPresent(faqPage.sideMenuIframe);
+            }, faqPage.iframeWaitTime);
+
+            // switch to sidebar menu iframe
+            browser.switchTo().frame(browser.findElement(
+                By.css('.cms_sideframe-frame iframe')));
+
+            browser.wait(function () {
+                return browser.isElementPresent(faqPage.pagesLink);
+            }, faqPage.mainElementsWaitTime);
+
+            faqPage.pagesLink.click();
+
+            // check if the page already exists and return the status
+            return faqPage.addPageLink.isPresent();
+        }).then(function (present) {
+            if (present === true) {
+                // page is absent - create new page
+                browser.wait(function () {
+                    return browser.isElementPresent(faqPage.addPageLink);
+                }, faqPage.mainElementsWaitTime);
+
+                faqPage.addPageLink.click();
+
+                browser.wait(function () {
+                    return browser.isElementPresent(faqPage.titleInput);
+                }, faqPage.mainElementsWaitTime);
+
+                faqPage.titleInput.sendKeys('Test').then(function () {
+                    faqPage.saveButton.click();
+
+                    faqPage.slugErrorNotification.isPresent()
+                        .then(function (present) {
+                        if (present === false) {
+                            browser.wait(function () {
+                                return browser.isElementPresent(faqPage.editPageLink);
+                            }, faqPage.mainElementsWaitTime);
+
+                            // wait till the editPageLink will become clickable
+                            browser.sleep(500);
+
+                            // validate/click edit page link
+                            faqPage.editPageLink.click();
+
+                            // switch to default page content
+                            browser.switchTo().defaultContent();
+
+                            browser.wait(function () {
+                                return browser.isElementPresent(faqPage.testLink);
+                            }, faqPage.mainElementsWaitTime);
+
+                            // validate test link text
+                            faqPage.testLink.getText()
+                                .then(function (title) {
+                                expect(title).toEqual('Test');
+                            });
+                        }
+                    });
+                });
+            }
         });
     });
 
