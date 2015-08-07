ece73d4cf084ae942cda5fcb58fa329f02b035e2
Martin Koistinen <mkoistinen@gmail.com>
2015-08-07 00:53:13 -0400

    Bump to version 1.0.4

--- aldryn_faq/__init__.py
+++ aldryn_faq/__init__.py
@@ -2,6 +2,8 @@
 
 from __future__ import unicode_literals
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
+
+# TODO: Check if these are still required
 request_faq_category_identifier = 'aldryn_faq_current_category'
 request_faq_question_identifier = 'aldryn_faq_current_question'
