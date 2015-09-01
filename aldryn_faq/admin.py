68a8df43970d65fd28e777ad6cde1551ccc29d43
Martin Koistinen <mkoistinen@gmail.com>
2015-09-01 16:21:39 -0400

    Fixes tags column in change-list, also, fixes 1.8 tests

--- aldryn_faq/admin.py
+++ aldryn_faq/admin.py
@@ -60,10 +60,18 @@ class QuestionAdmin(VersionedPlaceholderAdminMixin,
                     TranslatableAdmin):
 
     render_placeholder_language_tabs = False
-    list_display = ['__str__', 'category', 'tags', 'is_top', 'number_of_visits']
+    list_display = ['__str__', 'category', 'tag_list', 'is_top', 'number_of_visits']
     list_filter = ['category', 'translations__language_code']
     frontend_editable_fields = ('title', 'category', 'answer_text')
     readonly_fields = ['number_of_visits']
+
+    def tag_list(self, obj):
+        """
+        Displays Taggit tags to a comma-separated list of the tags’ names.
+        """
+        return ", ".join([str(tag) for tag in obj.tags.get_query_set()])
+    tag_list.short_description = 'Tags'
+    tag_list.allow_tags = True
 
     def get_fieldsets(self, request, obj=None):
         fieldsets = [
