212ebb24274569ceac007538e455bcca234bdc08
Martin Koistinen <mkoistinen@gmail.com>
2015-09-03 21:50:18 -0400

    Added appconfig options for permalink handling

--- aldryn_faq/cms_appconfig.py
+++ aldryn_faq/cms_appconfig.py
@@ -10,6 +10,23 @@ from aldryn_reversion.core import version_controlled_content
 from cms.models.fields import PlaceholderField
 from parler.models import TranslatableModel, TranslatedFields
 
+# Different styles of slugs supported where 1 is the category PK and 2 is the
+# question PK
+PERMALINK_CHOICES = (
+    ('Pp', _('1/2/', )),
+    ('Ps', _('1/question-slug/', )),
+    ('Sp', _('category-slug/2/', )),
+    ('Ss', _('category-slug/question-slug/', )),  # Default
+    ('Bp', _('1-category-slug/2/', )),
+    ('Bs', _('1-category-slug/question-slug/', )),
+)
+
+NON_PERMALINK_HANDLING = (
+    (200, _('Allow')),
+    (302, _('Redirect to permalink (default)')),  # Default
+    (301, _('Permanent redirect to permalink')),
+    (404, _('Return 404: Not Found')),
+)
 
 @version_controlled_content()
 class FaqConfig(TranslatableModel, AppHookConfig):
@@ -17,6 +34,17 @@ class FaqConfig(TranslatableModel, AppHookConfig):
     translations = TranslatedFields(
         app_title=models.CharField(_('application title'), max_length=234),
     )
+
+    permalink_type = models.CharField(_('permalink type'), max_length=2,
+        blank=False, choices=PERMALINK_CHOICES, default="Ss",
+        help_text=_('Choose the style of urls to use from the examples. '
+                    '(Note, all types are relative to apphook)'))
+
+    non_permalink_handling = models.SmallIntegerField(
+        _('non-permalink handling'),
+        blank=False, default=302,
+        choices=NON_PERMALINK_HANDLING,
+        help_text=_('How to handle non-permalink urls?'))
 
     # Category List PHFs
     placeholder_faq_sidebar_top = PlaceholderField(
