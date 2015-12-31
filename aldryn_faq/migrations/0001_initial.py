52cd3408327a245819fad4476bf322dd9c69994e
Kirill Kniazev <kirill.kniazev@divio.ch>
2015-12-31 18:19:58 +0200

    Default FaqConfig: Django 1.7 migrations.

--- aldryn_faq/migrations/0001_initial.py
+++ aldryn_faq/migrations/0001_initial.py
@@ -1,12 +1,70 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.conf import settings
+from django.db import models, migrations, transaction
+from django.db.models import get_model
+from django.db.utils import ProgrammingError, OperationalError
 import aldryn_translation_tools.models
 import app_data.fields
 import cms.models.fields
 import sortedm2m.fields
 import djangocms_text_ckeditor.fields
+
+APP_PACKAGE = 'aldryn_faq'
+APP_CONFIG = 'FaqConfig'
+DEFAULT_NAMESPACE = 'aldryn_faq_default'
+DEFAULT_APP_TITLE = 'Default FAQ'
+
+
+def noop(apps, schema_editor):
+    pass
+
+
+def get_config_count(model_class):
+    with transaction.atomic():
+        count = model_class.objects.count()
+    return count
+
+
+def create_default_config(apps, schema_editor):
+    FaqConfig = apps.get_model(APP_PACKAGE, APP_CONFIG)
+
+    # if we try to execute this migration after cms migrations were migrated
+    # to latest - we would get an exception because apps.get_model
+    # contains cms models in the last known state (which is the dependency
+    # migration state). If that is the case we need to import the real model.
+    try:
+        # to avoid the following error:
+        #   django.db.utils.InternalError: current transaction is aborted,
+        #   commands ignored until end of transaction block
+        # we need to cleanup or avoid that by making transaction atomic.
+        count = get_config_count(FaqConfig)
+    except (ProgrammingError, OperationalError):
+        model_path = '{0}.{1}'.format(APP_PACKAGE, APP_CONFIG)
+        FaqConfig = get_model(model_path)
+        count = get_config_count(FaqConfig)
+
+    if not count == 0:
+        return
+    # create only if there is no configs because user may already have
+    # existing and configured config.
+    app_config = FaqConfig(namespace=DEFAULT_NAMESPACE)
+    # usually generated in aldryn_apphooks_config.models.AppHookConfig
+    # but in migrations we don't have real class with correct parents.
+    app_config.type = '{0}.cms_appconfig.{1}'.format(APP_PACKAGE, APP_CONFIG)
+    # placeholders
+    # cms checks if instance.pk is set, and if it isn't cms creates a new
+    # placeholder but it does that with real models, and fields on instance
+    # are faked models. To prevent that we need to manually set instance pk.
+    app_config.pk = 1
+    app_config.save()
+
+    # translations
+    app_config_translation = app_config.translations.create()
+    app_config_translation.language_code = settings.LANGUAGES[0][0]
+    app_config_translation.app_title = DEFAULT_APP_TITLE
+    app_config_translation.save()
 
 
 class Migration(migrations.Migration):
@@ -197,4 +255,6 @@ class Migration(migrations.Migration):
             field=models.ForeignKey(verbose_name='appconfig', blank=True, to='aldryn_faq.FaqConfig', null=True),
             preserve_default=True,
         ),
+        # create default FaqConfig
+        migrations.RunPython(create_default_config, noop)
     ]
