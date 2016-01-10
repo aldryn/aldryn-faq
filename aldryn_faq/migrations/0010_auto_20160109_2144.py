5c54322fbe711d7d4ceb2fbe3db1d86b6832d62e
Martin Koistinen <mkoistinen@gmail.com>
2016-01-09 21:46:52 -0500

    Ensure migrations are up-to-date

---
+++ aldryn_faq/migrations/0010_auto_20160109_2144.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import app_data.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('aldryn_faq', '0009_auto_20160107_1043'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='faqconfig',
+            name='app_data',
+            field=app_data.fields.AppDataField(default=b'{}', editable=False),
+        ),
+    ]
