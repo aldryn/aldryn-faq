e54bfff89e4b97a0b7631e917e455319c8785b13
Paulo <commonzenpython@gmail.com>
2016-05-23 15:36:35 -0400

    cms wizard only available on >= 3.2

--- aldryn_faq/tests/test_wizards.py
+++ aldryn_faq/tests/test_wizards.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+import sys
+
+from distutils.version import LooseVersion
+
+import cms
+
 from djangocms_helper.utils import create_user
 
 from aldryn_faq.cms_wizards import CreateFaqQuestionForm
@@ -6,6 +12,16 @@ from aldryn_faq.cms_wizards import CreateFaqQuestionForm
 from .test_base import AldrynFaqTest
 
 
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# The CMS wizard system was introduced in 3.2.0
+CMS_3_2 = LooseVersion(cms.__version__) >= LooseVersion('3.2.0')
+
+
+@unittest.skipUnless(CMS_3_2, "No wizard support in CMS < 3.2")
 class TestFAQWizard(AldrynFaqTest):
 
     def setUp(self):
