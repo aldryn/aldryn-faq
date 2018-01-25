51631e093c286bc4662f1ee9dfec32cf515430d9
Filipe Waitman <filwaitman@gmail.com>
2018-01-25 15:14:17 -0200

    Bumped version to 2.0.0

--- aldryn_faq/__init__.py
+++ aldryn_faq/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import unicode_literals
 
-__version__ = '2.0.0rc2'
+__version__ = '2.0.0'
 
 # TODO: Check if these are still required
 request_faq_category_identifier = 'aldryn_faq_current_category'
